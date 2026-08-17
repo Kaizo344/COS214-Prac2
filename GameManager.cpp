9ed40ee703b7629dfd1cd2c6ec967e042a53ba55
Motheo-mto <motsemmemotheo@gmail.com>
2026-08-17 15:08:43 +0200

    Implemented the GameManager

---
+++ GameManager.cpp
@@ -0,0 +1,49 @@
+#include "GameManager.h"
+#include <iostream>
+
+GameManager::GameManager() : trip(nullptr), traveller(nullptr), worldRoot(nullptr), biomeFactory(nullptr)
+{}
+
+GameManager::~Gamemanager()
+{
+    if(trip != nullptr)
+    {
+        delete trip;
+        trip = nullptr;
+    }
+    if(traveller != nullptr)
+    {
+        delete traveller;
+        traveller = nullptr;
+    }
+    if(worldRoot != nullptr)
+    {
+        delete worlRoot;
+        worldRoot = nullptr;
+    }
+    if(biomeFactory != nullptr)
+    {
+        delete biomeFactory;
+        biomeFactory = nullptr;
+    }
+}
+
+void GameManager::setBiome(BiomeFactory* f)
+{
+    if(biomeFactory != nullptr)
+    {
+        delete biomeFactory;
+    }
+    biomeFactory = f;
+}
+
+void GameManager::run() {
+    std::cout << "Wayfarer Engine Initializing..." << std::endl;
+    
+    if (!trip || !traveller || !worldRoot || !biomeFactory) {
+        std::cout << "Error: GameManager cannot run." << std::endl;
+        return;
+    }
+    std::cout << "Starting the main game." << std::endl;
+    
+}
---
+++ GameManager.h
@@ -0,0 +1,23 @@
+#ifndef GAMEMANAGER_H
+#define GAMEMANAGER_H
+
+#include "Trip.h"
+#include "Traveller.h"
+#include "Region.h"
+#include "BiomeFactory.h"
+
+class GameManager
+{
+    private:
+        Trip* trip;
+        Traveller* traveller;
+        Region* worldRoot;
+        BiomeFactory* biomeFactory;
+    public:
+        GameManager();
+        ~GameManager();
+        void run();
+        void setBiome();
+};
+
+#endif // GAMEMANAGER_H
